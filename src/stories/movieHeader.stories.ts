ea21133ba76e269e1e75e5a69ebd19a370be688a
Martin <20106234@mail.wit.ie>
2024-03-11 18:16:16 +0100

    Movie Details  page components.

--- src/stories/movieHeader.stories.ts
+++ src/stories/movieHeader.stories.ts
@@ -1,15 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import MovieDetails from "../components/movieDetails";
+import MovieHeader from "../components/headerMovie";
 import SampleMovie from "./sampleData";
 
 const meta = {
-  title: "Movie Details Page/MovieDetails",
-  component: MovieDetails,
-} satisfies Meta<typeof MovieDetails>;
+    title: "Movie Details Page/MovieHeader",
+    component: MovieHeader,
+} satisfies Meta<typeof MovieHeader>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 export const Basic: Story = {
-  args:  SampleMovie
+    args: SampleMovie
 };
 Basic.storyName = "Default";
