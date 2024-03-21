1d151b73128ec4881bdda17053ad5a3dbf6dfc7f
Martin <102468994+MartinL-no@users.noreply.github.com>
2024-03-21 19:27:12 +0100

    Show movie review excerpts; Refactor Details page.

--- src/types/interfaces.d.ts
+++ src/types/interfaces.d.ts
@@ -52,3 +52,9 @@ export interface MovieListPageTemplateProps {
   title: string;
   selectFavourite: (m: number) => void;
 }
+
+export interface Review {
+  id: string;
+  content: string;
+  author: string;
+}
