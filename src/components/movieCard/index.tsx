3585be1593f64d65ec62088f1a651df98d65a6c4
Martin <102468994+MartinL-no@users.noreply.github.com>
2024-04-30 19:21:58 +0200

    Refactor: Favourite movie list now stored in a context.

--- src/components/movieCard/index.tsx
+++ src/components/movieCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { MouseEvent } from "react";
+import React, { MouseEvent, useContext } from "react";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -13,8 +13,9 @@ import StarRateIcon from "@mui/icons-material/StarRate";
 import Grid from "@mui/material/Grid";
 import IconButton from "@mui/material/IconButton";
 import img from "../../images/film-poster-placeholder.png";
-import { BaseMovie } from "../../types/interfaces";
+import { ListedMovie } from "../../types/interfaces";
 import { Link } from "react-router-dom";
+import { MoviesContext } from "../../contexts/moviesContext";
 
 const styles = {
   card: { maxWidth: 345 },
@@ -24,14 +25,15 @@ const styles = {
   },
 };
 
-interface MovieCardProps extends BaseMovie {
-  selectFavourite: (movieId: number) => void;
-}
+const MovieCard: React.FC<ListedMovie> = (props) => {
+  const movie = { ...props, favourite: false };
+  const { favourites, addToFavourites } = useContext(MoviesContext);
 
-const MovieCard: React.FC<MovieCardProps> = (props) => {
+  if (favourites.find((id) => id === movie.id)) movie.favourite = true;
+
   const handleAddToFavourite = (e: MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    props.selectFavourite(props.id);
+    addToFavourites(movie);
   };
 
   return (
